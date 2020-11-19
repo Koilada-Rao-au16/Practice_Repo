6be8a9ab2cbbbfb832edc3a2b25b034402f46ef5
unknown <bhaskar9.koilada@gmail.com>
2020-11-19 19:59:55 +0530

    operators

---
+++ week02/day04/operators_practice.py
@@ -0,0 +1,47 @@
+#Arthematic operators
+
+a = 5
+b = 6
+
+print(a + b)
+
+print(a - b)
+
+print (a // b)
+
+#Basic Calculator
+
+# print ("hey !! use my calculator")
+
+# print ("enter your 1st no")
+# no1 = float(input())
+
+# print ("enter your 2nd no")
+# no2 = float(input())
+
+# print(no1 * no2)
+
+
+#Assignment operator
+
+a = 1
+b = 2
+
+print(a == b) 
+
+print (a != b)
+
+print ("bhaskar == bhaskar")
+
+#comparision
+
+a = 2
+
+b = 5
+
+print (a >= b)
+
+
+a += 10
+
+print (a)
